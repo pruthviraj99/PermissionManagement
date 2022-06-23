4af79e8303f7b39e0d189cdae143adffcb0c8abd
pruthviraj99 <107990576+pruthviraj99@users.noreply.github.com>
2022-06-23 13:13:39 +0530

    Revert "- Added .kts file and update based on KTS Extension rulse base on bui…"

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("com.android.application") version ProjectBuildGradleDependency.applicationVersion apply false
-    id("com.android.library") version ProjectBuildGradleDependency.androidLibraryVersion apply false
-    id("org.jetbrains.kotlin.android") version ProjectBuildGradleDependency.kotlinLibraryVersion apply false
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir)
-}
